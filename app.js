4ae6dad4e5d2ea5f5103560a6b423ce1468a990e
Aalaizha <l.bnjamin0309@gmail.com>
2022-08-28 16:50:33 -0400

    Add all form information to renderInfo()

--- app.js
+++ app.js
@@ -2,22 +2,15 @@ import { CreateToDoItem } from "./scripts/createToDo.js";
 import { create } from "./scripts/DOM.js";
 
 let button = document.querySelector(".createButton");
-let nameField = document.querySelector("#taskName");
-let dateField = document.querySelector("#taskDueDate");
-let priorityField = document.querySelector("#taskPriority");
-let descriptionField = document.querySelector("#taskDescription");
-let myARray = [];
-let removeBtn = document.querySelector(".removeBtn");
-
 button.addEventListener("click", renderInfo);
 
 //renderInfo() gets the inputted value from the text field and stores it as nameText. A new variable called newItem stores a new CreateToDoItem which has the nameText as its arguments. create() is then called.
 
 function renderInfo() {
-  let nameText = nameField.value;
-  let dateText = dateField.value;
-  let priorityText = priorityField.value;
-  let descriptionText = descriptionField.value;
+  let nameText = document.querySelector("#taskName").value;
+  let dateText = document.querySelector("#taskDueDate").value;
+  let priorityText = document.querySelector("#taskPriority").value;
+  let descriptionText = document.querySelector("#taskDescription").value;
 
   let newItem = new CreateToDoItem(
     nameText,
@@ -29,3 +22,5 @@ function renderInfo() {
 
   document.getElementById("form").reset();
 }
+
+
