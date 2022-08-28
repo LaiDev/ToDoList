3e6189cc1a479711b534886286ff2312d80d00cc
Aalaizha <l.bnjamin0309@gmail.com>
2022-08-28 16:49:24 -0400

    Add class to DOM objects

--- scripts/DOM.js
+++ scripts/DOM.js
@@ -5,23 +5,27 @@ let container = document.querySelector(".toDoCardsContainer");
 export function create(itemTitle, itemDueDate, itemPriority, itemDescription) {
   let cardDiv = document.createElement("div");
   cardDiv.classList.add("toDoCard");
-
   container.append(cardDiv);
 
   let title = document.createElement("p");
   title.innerHTML = itemTitle;
+  title.classList.add("cardTitle");
   cardDiv.append(title);
+
   let dueDate = document.createElement("p");
   dueDate.innerHTML = itemDueDate;
+  dueDate.classList.add("cardDueDate");
   cardDiv.append(dueDate);
+
   let priority = document.createElement("p");
   priority.innerHTML = itemPriority;
+  priority.classList.add("cardPriority");
   cardDiv.append(priority);
+
   let description = document.createElement("p");
   description.innerHTML = itemDescription;
+  description.classList.add("cardDescription");
   cardDiv.append(description);
-  let removeBtn = document.createElement("button");
-  removeBtn.innerHTML = "Remove";
-  removeBtn.classList.add("removeBtn");
-  cardDiv.append(removeBtn);
+
+
 }
