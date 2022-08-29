1278379f25e2b007046c6564342cd5c1ca413efc
Aalaizha <l.bnjamin0309@gmail.com>
2022-08-28 23:26:53 -0400

    Add styling to task cards based on priority

--- scripts/DOM.js
+++ scripts/DOM.js
@@ -1,9 +1,7 @@
+let myArray = [];
 let name = "Aalaizha";
 let WelcomeMessage = document.querySelector(".welcomeMessage");
 WelcomeMessage.innerHTML = `Welcome, ${name}`;
-
-let tasks = document.querySelector(".tasksGrid");
-console.log(tasks.length)
 
 const createTaskButton = document.querySelector(".addTaskButton");
 
@@ -15,30 +13,62 @@ createTaskButton.addEventListener("click", function () {
 
 let container = document.querySelector(".tasksGrid");
 
+//Card div is a flex item. Top part is divided into two divs
+console.log(myArray.length);
+
 export function create(itemTitle, itemDueDate, itemPriority, itemDescription) {
   let cardDiv = document.createElement("div");
   cardDiv.classList.add("toDoCard");
   container.append(cardDiv);
 
+  //div holding left and right
+  let topCard = document.createElement("div");
+  topCard.classList.add("topCardContainer");
+  cardDiv.append(topCard);
+
+  //Div containing the items on the right side of the top of the card
+  let rightTop = document.createElement("div");
+  rightTop.classList.add("topRight");
+  topCard.append(rightTop);
+
+  //Div containing the items on the left side of the top of the card
+  let leftTop = document.createElement("div");
+  leftTop.classList.add("topLeft");
+  topCard.append(leftTop);
+
   let title = document.createElement("p");
   title.innerHTML = itemTitle;
   title.classList.add("cardTitle");
-  cardDiv.append(title);
+  leftTop.append(title);
 
   let dueDate = document.createElement("p");
   dueDate.innerHTML = itemDueDate;
   dueDate.classList.add("cardDueDate");
-  cardDiv.append(dueDate);
+  rightTop.append(dueDate);
 
   let priority = document.createElement("p");
   priority.innerHTML = itemPriority;
   priority.classList.add("cardPriority");
-  cardDiv.append(priority);
+ 
+
+  switch (priority.innerHTML) {
+    case "Low":
+      cardDiv.style.backgroundColor = '#deffbf'
+      break;
+    case "Medium":
+      cardDiv.style.backgroundColor = "#ffffcc";
+      break;
+    case "High":
+      cardDiv.style.backgroundColor = "#ffcccc";
+      break;
+  }
 
   let description = document.createElement("p");
   description.innerHTML = itemDescription;
   description.classList.add("cardDescription");
   cardDiv.append(description);
 
-
+  myArray.push(this);
+  let totalTaskMessage = document.querySelector(".totalTaskMessage");
+  totalTaskMessage.innerHTML = `Total Tasks: ${myArray.length}`;
 }
